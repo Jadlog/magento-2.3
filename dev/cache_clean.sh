d2b4620082b852e7c46828e48318a59864798e42
Sistemas - Jadlog <sistemas@jadlog.com.br>
2019-09-27 19:39:26 -0300

    adicionar opção "compile" no script cache_clean.sh

--- dev/cache_clean.sh
+++ dev/cache_clean.sh
@@ -2,5 +2,8 @@ sudo rm -rf /var/www/html/app/code/Jadlog
 sudo cp -Rf /src/app/* /var/www/html/app/
 sudo chown -R www-data.www-data /var/www/html
 sudo php /var/www/html/bin/magento cache:clean
+if [ "$1" = "compile" ]; then
+  sudo php /var/www/html/bin/magento setup:di:compile
+fi
 sudo chown -R www-data.www-data /var/www/html
 
